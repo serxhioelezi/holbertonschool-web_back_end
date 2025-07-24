ce0244a557a2a39880ee6edc169142acdcad01c4
serxhioelezi <serxhioelezi6@gmail.com>
2025-07-24 19:21:57 +0200

    12th exercise

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,3 +1,11 @@
 export default function createReportObject(employeesList) {
 
+    return {
+        allEmployees: {
+            ...employeesList,
+        },
+        getNumberofDepartments(employeesList) {
+            return Object.keys(employeesList).length;
+        },
+    };
 }
