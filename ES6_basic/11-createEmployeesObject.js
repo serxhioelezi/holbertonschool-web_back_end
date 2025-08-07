bbf1b8948610b4d7775322c06cbefabcfd1d5f57
serxhioelezi <serxhioelezi6@gmail.com>
2025-08-07 11:21:07 +0000

    basic exercises

--- ES6_basic/11-createEmployeesObject.js
+++ ES6_basic/11-createEmployeesObject.js
@@ -1,6 +1,5 @@
 export default function createEmployeesObject(departmentName, employees) {
-    const obj = {
-        [departmentName]: employees,
-    };
-    return obj;
+  return {
+    [departmentName]: [...employees],
+  };
 }
