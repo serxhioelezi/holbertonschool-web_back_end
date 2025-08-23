e36b0f856f07d53c1ae635139444dd41f1b1ccf8
serxhioelezi <serxhioelezi6@gmail.com>
2025-08-23 13:16:39 +0200

    classes

---
+++ ES6_classes/2-hbtn_course.js
@@ -0,0 +1,60 @@
+export default class HolbertonCourse {
+
+    constructor(name,length,students) {
+        if(typeof name === 'string' ){
+            this._name = name;
+        } else{
+            throw new Error('Name must be a string')
+        }
+
+        if(typeof length === 'number'){
+            this._length = length;
+        }else{
+            throw new Error('Length must be a number')
+        }
+        
+        if(Array.isArray(students)){
+            this._students = students;
+        } else{
+            throw new Error('Students must be an array of strings')
+        }
+    }
+
+    get name() {
+        return this._name;
+    }
+
+    get length() {
+        return this._length;
+    }
+
+    get students() {
+        return this._students;
+    }
+
+    set name(name) {
+        if(typeof name === 'string' ){
+            return this._name = name;
+        } else{
+            throw new Error('Name must be a string')
+        }
+        
+    }
+
+    set length(length) {
+        if(typeof length === 'number'){
+            return this._length = length;
+        }else{
+            throw new Error('Length must be a number')
+        }
+    }
+
+    set students(students) {
+        if(Array.isArray(students)){
+            return this._students = students;
+        } else{
+            throw new Error('Students must be an array of strings')
+        }
+        
+    }
+}
