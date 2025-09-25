cf8a260bdb635394e8e1ebbad54c588816f0e505
serxhioelezi <serxhioelezi6@gmail.com>
2025-09-25 14:29:13 +0200

    mysql advanced

--- MySQL_Advanced/0-uniq_users.sql
+++ MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
--- write a SQL script that creates a table users
+-- Create the users table if it doesn't already exist
 CREATE TABLE IF NOT EXISTS users (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
     name VARCHAR(255)
-)
+);
--- MySQL_Advanced/1-country_users.sql
+++ MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,8 @@
--- write a SQL script that creates a table users
+-- Create table users
 CREATE TABLE IF NOT EXISTS users (
-    id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-    email VARCHAR(255) NOT NULL UNIQUE,
-    name VARCHAR(255),
-    country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US' 
-)
+        id int NOT NULL AUTO_INCREMENT,
+        email varchar(255) NOT NULL UNIQUE,
+        name varchar(255),
+        country ENUM('US', 'CO', 'TN') NOT NULL DEFAULT 'US',
+        PRIMARY KEY (id)
+);
